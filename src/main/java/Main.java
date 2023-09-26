d5e2e690202141c1b5cdb43ce904d94115043e59
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-09-26 21:24:31 +0200

    151. Reverse words in a string

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,8 @@
-import _1_999._300_399._345_Reverse_Vowels_of_a_String;
+import _1_999._100_200._151_Reverse_Words_in_a_String;
 
 public class Main {
     public static void main(String[] args) {
 
-        _345_Reverse_Vowels_of_a_String.show("hello");
+        _151_Reverse_Words_in_a_String.show("hello world from Serbia");
     }
 }
---
+++ src/main/java/_1_999/_100_200/_151_Reverse_Words_in_a_String.java
@@ -0,0 +1,20 @@
+package _1_999._100_200;
+
+public class _151_Reverse_Words_in_a_String {
+
+    public static void show(String s) {
+        System.out.println(reverseWords(s));
+    }
+
+
+    public static String reverseWords(String s) {
+        String[] arr = s.trim().split(" +");
+        StringBuilder sb = new StringBuilder();
+        for(int i = arr.length -1; i>= 0; i--) {
+            sb.append(arr[i]);
+            sb.append(" ");
+        }
+        return sb.toString().trim();
+    }
+
+}
